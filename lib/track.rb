8ce09306b1c4d4c6bfd6b2c931cdc891890be09b
William Marshall <w.marshall@elsevier.com>
2022-03-14 15:40:23 +0000

    Initial exercise setup

---
+++ lib/track.rb
@@ -0,0 +1,8 @@
+class Track
+  def initialize(title, artist) # title and artist are both strings
+  end
+
+  def matches?(keyword) # keyword is a string
+    # Returns true if the keyword matches either the title or artist
+  end
+end
